6de443eebb2b4edaeee22158952180348cb488ce
Shoukat Ali <shoukatmastoi997@gmail.com>
2026-09-13 18:37:26 +0500

    s

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+app=FastAPI()
+@app.get("/")
+def root():
+    return {"Yes bro, I am here to help you!"}
+@app.get("/hello")
+def hello(): 
+    return {"Hello bro, How are you?"}
